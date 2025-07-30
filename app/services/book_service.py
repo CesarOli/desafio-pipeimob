c9a0bd4eb83284f92518fa323e47194e2e97be20
César A. Oliveira <cesaraleoliveira@gmail.com>
2025-07-30 18:18:53 -0300

    refactor: separate business logic into service layer

---
+++ app/services/book_service.py
@@ -0,0 +1,33 @@
+from sqlalchemy.orm import Session
+from app.core.db.models.book import Book
+from app.api.v1.schemas.book import BookCreate, BookUpdate
+
+def get_books(db: Session):
+    return db.query(Book).all()
+
+def get_book_by_id(db: Session, book_id: int):
+    return db.query(Book).filter(Book.id == book_id).first()
+
+def create_book(db: Session, book: BookCreate):
+    db_book = Book(**book.dict())
+    db.add(db_book)
+    db.commit()
+    db.refresh(db_book)
+    return db_book
+
+def update_book(db: Session, book_id: int, book_update: BookUpdate):
+    db_book = get_book_by_id(db, book_id)
+    if db_book:
+        update_data = book_update.dict(exclude_unset=True)
+        for key, value in update_data.items():
+            setattr(db_book, key, value)
+        db.commit()
+        db.refresh(db_book)
+    return db_book
+
+def delete_book_by_id(db: Session, book_id: int):
+    db_book = get_book_by_id(db, book_id)
+    if db_book:
+        db.delete(db_book)
+        db.commit()
+    return db_book
